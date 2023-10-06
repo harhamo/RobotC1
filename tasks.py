1581a14d3c9b4c64434bd4b43565727251de77cb
harhamo <tomi.lahdenmaki@gmail.com>
2023-10-06 13:30:33 +0300

    locator-based update

--- tasks.py
+++ tasks.py
@@ -10,16 +10,10 @@ LOGIN_CREDENTIALS = "robotsparebin"
 
 page = browser.page()
 http = HTTP()
-pdf = PDF()
-excel = Files()
 
 @task
 def robot_spare_bin_python():
-    """Insert the sales data for the week and export it as a PDF"""
-    browser.configure(
-        slowmo = 100,
-        )
-    
+    """Insert the sales data for the week and export it as a PDF"""   
     open_the_intranet_website()
     log_in()
     download_excel_file()
@@ -30,24 +24,18 @@ def robot_spare_bin_python():
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
+    browser.configure(
+        slowmo = 100,
+    )
+
     browser.goto("https://robotsparebinindustries.com/")
 
 def log_in():
     """Fills in the login form and clicks the 'Log in' button"""
     secret = vault.get_secret(LOGIN_CREDENTIALS)
-    page.fill("#username", secret["username"])
-    page.fill("#password", secret["password"])
-    #page.fill("#username", "maria")
-    #page.fill("#password", "thoushallnotpass")
-    page.click("button:text('Log in')")
-
-def fill_and_submit_sales_form(sales_rep):
-    """Fills in the sales data and click the 'Submit' button"""
-    page.fill("#firstname", sales_rep["First Name"])
-    page.fill("#lastname", sales_rep["Last Name"])
-    page.fill("#salesresult", str(sales_rep["Sales"]))
-    page.select_option("#salestarget", str(sales_rep["Sales Target"]))
-    page.click("text=Submit")
+    page.get_by_label("username").fill(secret["username"])
+    page.get_by_label("password").fill(secret["password"])
+    page.get_by_role("button", name = ("Log in")).click()
 
 def download_excel_file():
     """Downloads excel file from the given URL"""
@@ -55,12 +43,20 @@ def download_excel_file():
 
 def fill_form_with_excel_data():
     """Read data from excel and fill in the sales form"""
+    excel = Files()
     excel.open_workbook("SalesData.xlsx")
     worksheet = excel.read_worksheet_as_table("data", header=True)
     excel.close_workbook()
-
     for row in worksheet:
         fill_and_submit_sales_form(row)
+
+def fill_and_submit_sales_form(sales_rep):
+    """Fills in the sales data and click the 'Submit' button"""
+    page.get_by_label("First name").fill(sales_rep["First Name"])
+    page.get_by_label("Last name").fill(sales_rep["Last Name"])
+    page.get_by_label("Sales result ($)").fill(str(sales_rep["Sales"]))
+    page.get_by_label("Sales target ($)").select_option(str(sales_rep["Sales Target"]))
+    page.get_by_role("button", name = ("Submit")).click()
 
 def collect_results():
     """Take a screenshot of the page"""
@@ -68,10 +64,11 @@ def collect_results():
 
 def export_as_pdf():
     """Export the data to a odf file"""
+    pdf = PDF()
     sales_result_html = page.locator("#sales-results").inner_html() 
     pdf.html_to_pdf(sales_result_html, "output/sales_results.pdf")
 
 def log_out():
     """Press the 'Log out' button"""
-    page.click("text=Log out")
+    page.get_by_role("button", name = "Log out").click()
 
